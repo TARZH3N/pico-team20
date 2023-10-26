bbc795269159a51a51bb124a56bf1be167917f2b
SethSIT <121926596+SethSIT@users.noreply.github.com>
2023-10-26 09:59:46 +0800

    Upload the web server for Pico W
    Code for Web Server

---
+++ WebServer/main.c
@@ -0,0 +1,39 @@
+#include "lwip/apps/httpd.h"
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+#include "lwipopts.h"
+#include "ssi.h"
+#include "cgi.h"
+
+//WIFI CONNECTION DETAILS
+const char WIFI_SSID[] = "Family1";
+const char WIFI_PASSWORD[] = "Familycsst1";
+
+//Main Function
+int main(){
+    stdio_init_all();
+
+    //Initialise the WIFI chip
+    cyw43_arch_init();
+
+    cyw43_arch_enable_sta_mode();
+
+    while(cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000)){
+        printf("Connecting...\n");
+    }
+    
+    printf("Connected!\n");
+
+    //Enable server
+    httpd_init();
+    printf("Server is initialised!");
+
+    // Configure SSI and CGI handler
+    ssi_init(); 
+    printf("SSI Handler initialised\n");
+    cgi_init();
+    printf("CGI Handler initialised\n");
+    
+    // Infinite loop
+    while(1);
+}
