96f29d295d12a5df6727d50c251212ff45520308
lestertann <lestertanyiteck@gmail.com>
2023-11-25 15:44:13 +0800

    Fixed the problem.

---
+++ build/CMakeFiles/pico20Proj.dir/home/ubuntu/pico-nexmon/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/pico20Proj.dir/home/ubuntu/pico-nexmon/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c.obj: \
+ /home/ubuntu/pico-nexmon/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c
