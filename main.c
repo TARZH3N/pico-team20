0eb6dc74e9fdab95cbb19cdfaaf03c2eb17a34e4
Charisee Yip <64692199+chariseeyip@users.noreply.github.com>
2023-11-28 22:26:12 +0800

    added comments

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+// Include necessary libraries and header files
+//
 #include "lwip/apps/httpd.h"
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
@@ -8,36 +10,59 @@
 #include "sd_card.h"
 #include "ff.h"
 
-//WIFI CONNECTION DETAILS
-const char WIFI_SSID[] = "TYMD1946";
-const char WIFI_PASSWORD[] = "wifi1111";
+// WiFi Network Credentials
+//
+const char g_wifi_ssid[] = "TYMD1946";
+const char g_wifi_password[] = "wifi1111";
 
+// Function to list files in a directory
+//
 void list_files(const char *path) {
+
     FRESULT fr;
     DIR dir;
     static FILINFO fno;
 
-    fr = f_opendir(&dir, path); // Open directory
+    // Open the directory
+    //
+    fr = f_opendir(&dir, path);
     if (fr != FR_OK) {
         printf("ERROR: Could not open directory (%d)\r\n", fr);
         return;
     }
 
-    while (true) {
-        fr = f_readdir(&dir, &fno); // Read a directory item
+    // Read and print each item in the directory
+    //
+    for(;;) {
+
+        // Read a directory item
+        //
+        fr = f_readdir(&dir, &fno);
+
         if (fr != FR_OK || fno.fname[0] == 0) {
-            break; // Break on error or end of directory
+
+            // Break on error or end of directory
+            //
+            break;
         }
-        if (fno.fattrib & AM_DIR) { // Check if it's a directory
+
+        // Check if the item is a directory
+        //
+        if (fno.fattrib & AM_DIR) {
             printf("Directory: %s\r\n", fno.fname);
         } else {
+
+            // Print the name of the file
+            //
             printf("File: %s\r\n", fno.fname);
         }
     }
 }
 
-//Main Function
+// Main Function
+//
 int main(){
+
     FRESULT fr;
     DIR dir;
     FATFS fs;
@@ -46,19 +71,26 @@ int main(){
     char buf[100];
     static FILINFO fno;
 
+    // Initialize standard I/O
+    //
     stdio_init_all();
 
-    //Initialise the WIFI chip
+    // Initialise the WiFi chip
     cyw43_arch_init();
 
+    // Enable station mode on the WiFi chip
+    //
     cyw43_arch_enable_sta_mode();
-    // Initialize SD card
+
+    // Initialize the SD card driver
+    //
     if (!sd_init_driver()) {
         printf("ERROR: Could not initialize SD card\r\n");
         while (true);
     }
 
-    // Mount drive
+    // Mount the SD card filesystem
+    //
     fr = f_mount(&fs, "0:", 1);
     if (fr != FR_OK) {
         printf("ERROR: Could not mount filesystem (%d)\r\n", fr);
@@ -66,26 +98,34 @@ int main(){
     }
 
     printf("SD DRIVER INITIALIZED!\n");
-    
+
+    // List files in the root directory of the SD card
+    //
     list_files("0:");
         
-    //Connecting to Wifi Network as set up above
-    while(cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000)){
+    // Connecting to Wifi Network as set up above
+    //
+    while(cyw43_arch_wifi_connect_timeout_ms(g_wifi_ssid, g_wifi_password, CYW43_AUTH_WPA2_AES_PSK, 30000)){
         printf("Connecting...\n");
     }
     
     printf("Connected!\n");
     
-    //Enable server
+    // Enable the HTTP server
+    //
     httpd_init();
     printf("Server is initialised!\n");
 
     // Configure SSI and CGI handler
+    // 
     ssi_init(); 
     printf("SSI Handler initialised\n");
     cgi_init();
     printf("CGI Handler initialised\n");
     
     // Infinite loop to keep the server running
+    //
     while(1);
 }
+
+/*** end of file ***/
